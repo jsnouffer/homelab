e20e44d00f0c795aeb63551fc315f573a14dde8f
Jason Snouffer <jsnouff@gmail.com>
2024-06-05 19:24:09 -0400

    Sunsetting k8up operator

--- kubernetes-deployments/storage/k8up/update-crds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION=k8up-4.7.0
-PWD=$(dirname "$0")
-
-# see readme - https://github.com/k8up-io/k8up/tree/master/charts/k8up
-mkdir -p ${PWD}/templates
-curl -L https://github.com/k8up-io/k8up/releases/download/${VERSION}/k8up-crd.yaml > ${PWD}/templates/k8up-crd.yaml
